a5a3a6884deda4af597126b151dcce011c2ad472
BaptistCromb <baptist.crombez@student.howest.be>
2026-05-22 09:33:38 +0200

    feat: added a check if mute music -> mute music stays

--- src/js/music.js
+++ src/js/music.js
@@ -26,6 +26,7 @@ if (document.title == "Take a path") {
   documentaudio.play();
 }
 if (document.title == "Collect the fish") {
+  const documentaudio = new Audio("../music/Music/WeGoDiving__NoVocalWebsite.mp3");
   const audiobutton = document.createElement("i");
   audiobutton.classList.add("bi");
   audiobutton.classList.add("bi-volume-up");
@@ -35,20 +36,30 @@ if (document.title == "Collect the fish") {
   audiobutton.style.left = "1rem";
   audiobutton.style.cursor = "pointer";
   audiobutton.style.color = "white";
+
   audiobutton.addEventListener("click", () => {
     if (audiobutton.classList.contains("bi-volume-up")) {
       audiobutton.classList.remove("bi-volume-up");
       audiobutton.classList.add("bi-volume-mute");
+      sessionStorage.setItem("Music", "Muted");
       documentaudio.pause();
     } else {
       audiobutton.classList.remove("bi-volume-mute");
       audiobutton.classList.add("bi-volume-up");
+      sessionStorage.removeItem("Music");
       documentaudio.play();
     }
   });
-  document.body.appendChild(audiobutton);
-  const documentaudio = new Audio("../music/Music/WeGoDiving__NoVocalWebsite.mp3");
+
+  if (localStorage.getItem("Music") == "Muted") {
+    audiobutton.classList.remove("bi-volume-up");
+    audiobutton.classList.add("bi-volume-mute");
+    documentaudio.pause();
+    console.log("Brooooo");
+  } else {
+    documentaudio.play();
+  }
   documentaudio.loop = true;
   documentaudio.volume = 0.2;
-  documentaudio.play();
+  document.body.appendChild(audiobutton);
 }
