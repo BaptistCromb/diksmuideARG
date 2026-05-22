f5efa734d67427e36fb4572b86a9cf5fc8b5ea49
BaptistCromb <baptist.crombez@student.howest.be>
2026-05-22 09:42:41 +0200

    feat: made "backgroundmusicfunction" with 2 parameters, link + volume

--- src/js/music.js
+++ src/js/music.js
@@ -1,32 +1,5 @@
-if (document.title == "Take a path") {
-  const audiobutton = document.createElement("i");
-  audiobutton.classList.add("bi");
-  audiobutton.classList.add("bi-volume-up");
-  audiobutton.style.fontSize = "3rem";
-  audiobutton.style.position = "fixed";
-  audiobutton.style.top = "1rem";
-  audiobutton.style.left = "1rem";
-  audiobutton.style.cursor = "pointer";
-  audiobutton.style.color = "white";
-  audiobutton.addEventListener("click", () => {
-    if (audiobutton.classList.contains("bi-volume-up")) {
-      audiobutton.classList.remove("bi-volume-up");
-      audiobutton.classList.add("bi-volume-mute");
-      documentaudio.pause();
-    } else {
-      audiobutton.classList.remove("bi-volume-mute");
-      audiobutton.classList.add("bi-volume-up");
-      documentaudio.play();
-    }
-  });
-  document.body.appendChild(audiobutton);
-  const documentaudio = new Audio("music/HisSadCry.mp3");
-  documentaudio.loop = true;
-  documentaudio.volume = 0.1;
-  documentaudio.play();
-}
-if (document.title == "Collect the fish") {
-  const documentaudio = new Audio("../music/Music/WeGoDiving__NoVocalWebsite.mp3");
+const backgroundmusicfunction = (link, volume) => {
+  const documentaudio = new Audio(link);
   const audiobutton = document.createElement("i");
   audiobutton.classList.add("bi");
   audiobutton.classList.add("bi-volume-up");
@@ -39,27 +12,35 @@ if (document.title == "Collect the fish") {
 
   audiobutton.addEventListener("click", () => {
     if (audiobutton.classList.contains("bi-volume-up")) {
-      audiobutton.classList.remove("bi-volume-up");
-      audiobutton.classList.add("bi-volume-mute");
       sessionStorage.setItem("Music", "Muted");
+      audiobutton.classList.remove("bi-volume-up");
+      audiobutton.classList.add("bi-volume-mute");
       documentaudio.pause();
     } else {
+      sessionStorage.removeItem("Music");
       audiobutton.classList.remove("bi-volume-mute");
       audiobutton.classList.add("bi-volume-up");
-      sessionStorage.removeItem("Music");
       documentaudio.play();
     }
   });
 
-  if (localStorage.getItem("Music") == "Muted") {
+  if (sessionStorage.getItem("Music") == "Muted") {
     audiobutton.classList.remove("bi-volume-up");
     audiobutton.classList.add("bi-volume-mute");
     documentaudio.pause();
-    console.log("Brooooo");
   } else {
     documentaudio.play();
   }
   documentaudio.loop = true;
-  documentaudio.volume = 0.2;
+  documentaudio.volume = volume;
   document.body.appendChild(audiobutton);
+};
+
+if (document.title == "Take a path") {
+  backgroundmusicfunction("music/HisSadCry.mp3", 0.1);
+} else if (document.title == "Collect the fish") {
+  backgroundmusicfunction("../music/Music/WeGoDiving__NoVocalWebsite.mp3", 0.2);
 }
+
+// "music/HisSadCry.mp3"
+// 0.1
